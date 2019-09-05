fd04597d62a20d3ed9e0bba64523594b5d7ceabf
Sofasmile <sofiya.dyakonova@ukr.net>
2019-09-05 16:21:41 +0300

    changed description

--- src/main/java/core/basesyntax/JavaStreamApi.java
+++ src/main/java/core/basesyntax/JavaStreamApi.java
@@ -7,70 +7,71 @@ import java.util.Optional;
 public class JavaStreamApi {
 
     /**
-     * 1. Дано: {@link List} of {@link Integer} numbers.
-     * Вернуть сумму нечетных числел или 0, если таких несуществует
+     * <p>1. Дано: List of Integer numbers.
+     * Вернуть сумму нечетных числел или 0, если таких несуществует</p>
      **/
     public Integer oddSum(List<Integer> numbers) {
         return null;
     }
 
     /**
-     * 2. Дана коллекция строк Arrays.asList(«a1», «a2», «a3», «a1»).
-     * Вернуть количество вхождений объекта «a1»
+     * <p>2. Дана коллекция строк List of String elements
+     * (пример: Arrays.asList(«a1», «a2», «a3», «a1»)).
+     * Вернуть количество вхождений объекта `element`</p>
      **/
     public Long elementCount(List<String> elements, String element) {
         return null;
     }
 
     /**
-     * 3. Дана коллекция строк Arrays.asList(«a1», «a2», «a3», «a1»).
-     * Вернуть Optional первого элемента коллекции
+     * <p>3. Дана коллекция строк List of String elements
+     * (пример: Arrays.asList(«a1», «a2», «a3», «a1»)).
+     * Вернуть Optional первого элемента коллекции</p>
      **/
     public Optional<String> firstElement(List<String> elements) {
         return null;
     }
 
     /**
-     * 4. Дана коллекция строк Arrays.asList(«a1», «a2», «a3», «a1»).
-     * Найти элемент в коллекции равный «a3» или кинуть ошибку NoSuchElementException
+     * <p>4. Дана коллекция строк List of String elements
+     * (приметр: Arrays.asList(«a1», «a2», «a3», «a1»)).
+     * Найти элемент в коллекции равный `element` или кинуть ошибку NoSuchElementException</p>
      **/
     public String findElement(List<String> elements, String element) {
         return null;
     }
 
     /**
-     * 5. Дана коллекция чисел Arrays.asList(6, 2, 3, 7, 2, 5)
-     * Отнимите от каждого элемента, который стоит на непарной позиции
-     * (имеет не парный индекс) 1
+     * <p>5. Дана коллекция чисел List of Integer numbers (пример: Arrays.asList(6, 2, 3, 7, 2, 5))
+     * Отнимите от каждого элемента, который стоит на непарной позиции (имеет не парный индекс) 1
      * и верните среднее арифметическое всех нечетных чисел или киньте ошибку
-     * NoSuchElementException
+     * NoSuchElementException</p>
      **/
     public Double averageSumOdd(List<Integer> numbers) {
         return null;
     }
 
     /**
-     * 6. Дана коллекция класс People (с полями name — имя, age — возраст, sex — пол),
+     * <p>6. Дана коллекция класс People (с полями name — имя, age — возраст, sex — пол),
      * вида Arrays.asList( new People(«Вася», 16, Sex.MAN),
      * new People(«Петя», 23, Sex.MAN),
      * new People(«Елена», 42, Sex.WOMEN),
      * new People(«Иван Иванович», 69, Sex.MAN)).
-     * Давайте посмотрим примеры как работать с таким классом:
-     * - Выбрать мужчин-военнообязанных (от 18 до 27 лет)
+     * Задача: Выбрать мужчин-военнообязанных (от `fromAge` до `toAge` лет)</p>
      **/
     public List<People> manSelectByAge(List<People> peopleList, int fromAge, int toAge) {
         return Collections.emptyList();
     }
 
     /**
-     * 6. Дана коллекция класс People (с полями name — имя, age — возраст, sex — пол),
+     * <p>6. Дана коллекция класс People (с полями name — имя, age — возраст, sex — пол),
      * вида Arrays.asList( new People(«Вася», 16, Sex.MAN),
      * new People(«Петя», 23, Sex.MAN),
      * new People(«Елена», 42, Sex.WOMEN),
      * new People(«Иван Иванович», 69, Sex.MAN)).
-     * Давайте посмотрим примеры как работать с таким классом:
-     * - Найти всех потенциально работоспособных людей в выборке
-     * (т.е. от 18 лет и учитывая что женщины выходят в 55 лет, а мужчина в 60)
+     * Задача: Найти всех потенциально работоспособных людей в выборке от fromAge до
+     * femaleToAge для женщин, и от fromAge до maleToAge для мужчин
+     * Пример: от 18 лет и учитывая что женщины выходят в 55 лет, а мужчина в 60</p>
      **/
     public List<People> workablePeople(int fromAge, int femaleToAge,
                                        int maleToAge, List<People> peopleList) {
@@ -78,11 +79,10 @@ public class JavaStreamApi {
     }
 
     /**
-     * 7. Дано коллекцию {@link List} of {@link People} peoples.
-     * Класс People (с полями name — имя, age — возраст, sex — пол,
-     *                          {@link List} of {@link Cat} cats -  кошки этого человека).
-     * Дано класс Cat (name- имя кошки, age - возраст кошки).
-     * Задача: вивести все имена кошек в которых хозяева это девушки старше 18 лет
+     * <p>7. Дано коллекцию List of peoples. Класс People (с полями name — имя, age — возраст,
+     * sex — пол, List of Cats -  кошки этого человека).
+     * Дано класс Cat (name - имя кошки, age - возраст кошки).
+     * Задача: вивести все имена кошек в которых хозяева это девушки старше 18 лет</p>
      **/
     public List<String> getCatsNames(List<People> peopleList, int femaleAge) {
         return Collections.emptyList();
