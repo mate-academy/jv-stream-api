3f62a953751d02e32430373b1ff7283e495568a0
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-09-11 16:28:57 +0300

    added solution for task with Stream API

--- src/main/java/core/basesyntax/JavaStreamApi.java
+++ src/main/java/core/basesyntax/JavaStreamApi.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
-import java.util.Collections;
+import core.basesyntax.People.Sex;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class JavaStreamApi {
 
@@ -11,80 +14,110 @@ public class JavaStreamApi {
      * Вернуть сумму нечетных числел или 0, если таких несуществует</p>
      **/
     public Integer oddSum(List<Integer> numbers) {
-        return null;
+        return numbers.stream()
+            .filter(n -> n % 2 != 0)
+            .reduce(Integer::sum)
+            .orElse(0);
     }
 
     /**
      * <p>2. Дана коллекция строк List of String elements
-     * (пример: Arrays.asList(«a1», «a2», «a3», «a1»)).
-     * Вернуть количество вхождений объекта `element`</p>
+     * (пример: Arrays.asList(«a1», «a2», «a3», «a1»)). Вернуть количество вхождений объекта
+     * `element`</p>
      **/
     public Long elementCount(List<String> elements, String element) {
-        return null;
+        return elements.stream()
+            .filter(s -> s.equals(element))
+            .count();
     }
 
     /**
      * <p>3. Дана коллекция строк List of String elements
-     * (пример: Arrays.asList(«a1», «a2», «a3», «a1»)).
-     * Вернуть Optional первого элемента коллекции</p>
+     * (пример: Arrays.asList(«a1», «a2», «a3», «a1»)). Вернуть Optional первого элемента
+     * коллекции</p>
      **/
     public Optional<String> firstElement(List<String> elements) {
-        return null;
+        return elements.stream().findFirst();
     }
 
     /**
      * <p>4. Дана коллекция строк List of String elements
-     * (приметр: Arrays.asList(«a1», «a2», «a3», «a1»)).
-     * Найти элемент в коллекции равный `element` или кинуть ошибку NoSuchElementException</p>
+     * (приметр: Arrays.asList(«a1», «a2», «a3», «a1»)). Найти элемент в коллекции равный `element`
+     * или кинуть ошибку NoSuchElementException</p>
      **/
     public String findElement(List<String> elements, String element) {
-        return null;
+        return elements.stream()
+            .filter(s -> s.equals(element))
+            .findAny()
+            .orElseThrow(NoSuchElementException::new);
     }
 
     /**
      * <p>5. Дана коллекция чисел List of Integer numbers (пример: Arrays.asList(6, 2, 3, 7, 2, 5))
-     * Отнимите от каждого элемента, который стоит на непарной позиции (имеет не парный индекс) 1
-     * и верните среднее арифметическое всех нечетных чисел или киньте ошибку
+     * Отнимите от каждого элемента, который стоит на непарной позиции (имеет не парный индекс) 1 и
+     * верните среднее арифметическое всех нечетных чисел или киньте ошибку
      * NoSuchElementException</p>
      **/
     public Double averageSumOdd(List<Integer> numbers) {
-        return null;
+        return IntStream.range(0, numbers.size())
+            .map(index -> {
+                if (index % 2 == 0) {
+                    return numbers.get(index);
+                }
+                return numbers.get(index) - 1;
+            })
+            .filter(number -> number % 2 != 0)
+            .mapToDouble(number -> number)
+            .average()
+            .orElseThrow(NoSuchElementException::new);
     }
 
     /**
      * <p>6. Дана коллекция класс People (с полями name — имя, age — возраст, sex — пол),
-     * вида Arrays.asList( new People(«Вася», 16, Sex.MAN),
-     * new People(«Петя», 23, Sex.MAN),
-     * new People(«Елена», 42, Sex.WOMEN),
-     * new People(«Иван Иванович», 69, Sex.MAN)).
-     * Задача: Выбрать мужчин-военнообязанных (от `fromAge` до `toAge` лет)</p>
+     * вида Arrays.asList( new People(«Вася», 16, Sex.MAN), new People(«Петя», 23, Sex.MAN), new
+     * People(«Елена», 42, Sex.WOMEN), new People(«Иван Иванович», 69, Sex.MAN)). Задача: Выбрать
+     * мужчин-военнообязанных (от `fromAge` до `toAge` лет)</p>
      **/
     public List<People> manSelectByAge(List<People> peopleList, int fromAge, int toAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+            .filter(people -> people.getSex() == Sex.MAN
+                && people.getAge() >= fromAge
+                && people.getAge() <= toAge)
+            .collect(Collectors.toList());
     }
 
     /**
      * <p>6. Дана коллекция класс People (с полями name — имя, age — возраст, sex — пол),
-     * вида Arrays.asList( new People(«Вася», 16, Sex.MAN),
-     * new People(«Петя», 23, Sex.MAN),
-     * new People(«Елена», 42, Sex.WOMEN),
-     * new People(«Иван Иванович», 69, Sex.MAN)).
-     * Задача: Найти всех потенциально работоспособных людей в выборке от fromAge до
-     * femaleToAge для женщин, и от fromAge до maleToAge для мужчин
-     * Пример: от 18 лет и учитывая что женщины выходят в 55 лет, а мужчина в 60</p>
+     * вида Arrays.asList( new People(«Вася», 16, Sex.MAN), new People(«Петя», 23, Sex.MAN), new
+     * People(«Елена», 42, Sex.WOMEN), new People(«Иван Иванович», 69, Sex.MAN)). Задача: Найти всех
+     * потенциально работоспособных людей в выборке от fromAge до femaleToAge для женщин, и от
+     * fromAge до maleToAge для мужчин Пример: от 18 лет и учитывая что женщины выходят в 55 лет, а
+     * мужчина в 60</p>
      **/
     public List<People> workablePeople(int fromAge, int femaleToAge,
-                                       int maleToAge, List<People> peopleList) {
-        return Collections.emptyList();
+            int maleToAge, List<People> peopleList) {
+        return peopleList.stream()
+            .filter(people -> (people.getSex() == Sex.MAN
+                && people.getAge() >= fromAge
+                && people.getAge() <= maleToAge)
+                || (people.getSex() == Sex.WOMEN
+                && people.getAge() >= fromAge
+                && people.getAge() <= femaleToAge))
+            .collect(Collectors.toList());
     }
 
     /**
      * <p>7. Дано коллекцию List of peoples. Класс People (с полями name — имя, age — возраст,
-     * sex — пол, List of Cats -  кошки этого человека).
-     * Дано класс Cat (name - имя кошки, age - возраст кошки).
-     * Задача: вивести все имена кошек в которых хозяева это девушки старше 18 лет</p>
+     * sex — пол, List of Cats -  кошки этого человека). Дано класс Cat (name - имя кошки, age -
+     * возраст кошки). Задача: вивести все имена кошек в которых хозяева это девушки старше 18
+     * лет</p>
      **/
     public List<String> getCatsNames(List<People> peopleList, int femaleAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+            .filter(people -> people.getSex() == Sex.WOMEN
+                && people.getAge() >= femaleAge)
+            .flatMap(people -> people.getCatList().stream())
+            .map(Cat::getName)
+            .collect(Collectors.toList());
     }
 }
