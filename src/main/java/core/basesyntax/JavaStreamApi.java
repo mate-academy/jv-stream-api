02aa21f745aa7dd0517c2008e5a7e98dfc1ba8ed
NikolayKatsai <alaska.rok123@gmail.com>
2019-09-12 11:47:22 +0300

    JavaStreamApi class finished

--- src/main/java/core/basesyntax/JavaStreamApi.java
+++ src/main/java/core/basesyntax/JavaStreamApi.java
@@ -5,7 +5,6 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class JavaStreamApi {
 
