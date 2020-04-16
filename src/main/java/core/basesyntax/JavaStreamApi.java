7ca6337505df230975ed186e3984c5c78b7329fa
VitaliiLeshchenko <411100253g@gmail.com>
2020-04-16 17:15:05 +0300

    removed useless import

--- src/main/java/core/basesyntax/JavaStreamApi.java
+++ src/main/java/core/basesyntax/JavaStreamApi.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
