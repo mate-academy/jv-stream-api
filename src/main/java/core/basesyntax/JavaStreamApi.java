f875faf82ab501a869af1c6fedbf5b1f86f0aa01
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-17 12:18:04 +0300

    exchanged mapToInt() to reduce() with orElse()

--- src/main/java/core/basesyntax/JavaStreamApi.java
+++ src/main/java/core/basesyntax/JavaStreamApi.java
@@ -14,8 +14,8 @@ public class JavaStreamApi {
     public Integer oddSum(List<Integer> numbers) {
         return numbers.stream()
                 .filter(x -> x % 2 != 0)
-                .mapToInt(Integer::intValue)
-                .sum();
+                .reduce(Integer::sum)
+                .orElse(0);
     }
 
     /**
