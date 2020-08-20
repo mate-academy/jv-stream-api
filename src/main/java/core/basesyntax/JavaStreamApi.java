5f584d54ecba90c186f8162cfd039e8dfd39f9d0
ROMASHSX <andriiromash@gmail.com>
2020-08-20 15:09:20 +0300

    all methods are implemented

--- src/main/java/core/basesyntax/JavaStreamApi.java
+++ src/main/java/core/basesyntax/JavaStreamApi.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class JavaStreamApi {
 
@@ -11,7 +13,10 @@ public class JavaStreamApi {
      * Вернуть сумму нечетных числел или 0, если таких несуществует</p>
      **/
     public Integer oddSum(List<Integer> numbers) {
-        return null;
+        return numbers
+                .stream()
+                .mapToInt((p -> p % 2 == 1 ? p : 0))
+                .sum();
     }
 
     /**
@@ -20,7 +25,10 @@ public class JavaStreamApi {
      * Вернуть количество вхождений объекта `element`</p>
      **/
     public Long elementCount(List<String> elements, String element) {
-        return null;
+        return elements
+                .stream()
+                .filter(element::equals)
+                .count();
     }
 
     /**
@@ -29,7 +37,9 @@ public class JavaStreamApi {
      * Вернуть Optional первого элемента коллекции</p>
      **/
     public Optional<String> firstElement(List<String> elements) {
-        return null;
+        return elements
+                .stream()
+                .findFirst();
     }
 
     /**
@@ -38,7 +48,10 @@ public class JavaStreamApi {
      * Найти элемент в коллекции равный `element` или кинуть ошибку NoSuchElementException</p>
      **/
     public String findElement(List<String> elements, String element) {
-        return null;
+        return elements.stream()
+                .filter(element::equals)
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
     }
 
     /**
@@ -48,7 +61,11 @@ public class JavaStreamApi {
      * NoSuchElementException</p>
      **/
     public Double averageSumOdd(List<Integer> numbers) {
-        return null;
+        return IntStream.range(0, numbers.size())
+                .map(n -> n % 2 == 1 ? numbers.get(n) - 1 : numbers.get(n))
+                .filter(n -> n % 2 == 1)
+                .average()
+                .orElseThrow(NoSuchElementException::new);
     }
 
     /**
@@ -60,7 +77,12 @@ public class JavaStreamApi {
      * Задача: Выбрать мужчин-военнообязанных (от `fromAge` до `toAge` лет)</p>
      **/
     public List<People> manSelectByAge(List<People> peopleList, int fromAge, int toAge) {
-        return Collections.emptyList();
+        return peopleList
+                .stream()
+                .filter(p -> p.getSex() == People.Sex.MAN
+                        && p.getAge() < toAge
+                        && p.getAge() > fromAge)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -75,7 +97,12 @@ public class JavaStreamApi {
      **/
     public List<People> workablePeople(int fromAge, int femaleToAge,
                                        int maleToAge, List<People> peopleList) {
-        return Collections.emptyList();
+        return peopleList
+                .stream()
+                .filter(p -> p.getSex() == People.Sex.MAN
+                        ? p.getAge() <= maleToAge && p.getAge() >= fromAge
+                        : p.getAge() <= femaleToAge && p.getAge() >= fromAge)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -85,6 +112,11 @@ public class JavaStreamApi {
      * Задача: вивести все имена кошек в которых хозяева это девушки старше 18 лет</p>
      **/
     public List<String> getCatsNames(List<People> peopleList, int femaleAge) {
-        return Collections.emptyList();
+        return peopleList
+                .stream()
+                .filter(p -> p.getSex().equals(People.Sex.WOMEN) && p.getAge() >= femaleAge)
+                .flatMap(p -> p.getCatList().stream())
+                .map(Cat::getName)
+                .collect(Collectors.toList());
     }
 }
