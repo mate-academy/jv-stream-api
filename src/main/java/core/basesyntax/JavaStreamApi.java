d17a6a2529d5e1710e17093005d549e44465d2a6
romandubovskyi <rdubowskij@gmail.com>
2020-08-31 16:04:34 +0300

    removed unneeded imports

--- src/main/java/core/basesyntax/JavaStreamApi.java
+++ src/main/java/core/basesyntax/JavaStreamApi.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class JavaStreamApi {
 
